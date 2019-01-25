9681af454de42d95d9cc0447dd6267b0672c3dbb
Bruno Casali <brunoocasali@gmail.com>
2019-01-25 16:30:26 -0200

    Rename project with a new name Resque roulette

---
+++ lib/resque-roulette.rb
@@ -0,0 +1,2 @@
+require 'resque'
+require File.expand_path('resque/plugins/roulette', File.dirname(__FILE__))
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require 'bundler'
 Bundler.setup
 
-require 'resque-unfairly'
+require 'resque-roulette'
 require 'rspec'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
 end
-
